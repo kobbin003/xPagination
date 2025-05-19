044ca38c04d0f57e350a1b6635906c6676d84cbd
kobbin003 <dkfeto@gmail.com>
2025-05-19 11:10:31 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [employees, setEmployees] = useState([]);
+	const [offset, setOffset] = useState(0);
+	const limit = 10;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const goPreviousoffset = () => {
+		if (offset >= 10) setOffset((prev) => prev - 10);
+	};
+
+	const goNextoffset = () => {
+		const maxoffset = Math.floor(employees.length / limit) * 10;
+
+		if (offset < maxoffset) setOffset((prev) => prev + 10);
+	};
+
+	const currentPageEmployees = employees.slice(offset, offset + limit);
+	const currentPage = Math.floor(offset / 10) + 1;
+	useEffect(() => {
+		(async function () {
+			const url =
+				"https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+
+			try {
+				const res = await fetch(url);
+				const data = await res.json();
+				if (data) {
+					setEmployees(data);
+				}
+			} catch (error) {
+				console.error(error);
+				alert("failed to fetch data");
+			}
+		})();
+	}, []);
+
+	return (
+		<main>
+			<h1>Employee Data Table</h1>
+			<table>
+				<thead>
+					<tr>
+						<th align="left">ID</th>
+						<th align="left">Name</th>
+						<th align="left">Email</th>
+						<th align="left">Role</th>
+					</tr>
+				</thead>
+				<tbody>
+					{currentPageEmployees &&
+						currentPageEmployees.map(({ id, name, email, role }) => (
+							<tr key={id}>
+								<td>{id}</td>
+								<td>{name}</td>
+								<td>{email}</td>
+								<td>{role}</td>
+							</tr>
+						))}
+				</tbody>
+			</table>
+			<div className="pagination-control">
+				<button onClick={goPreviousoffset}>Previous</button>
+				<div>{currentPage}</div>
+				<button onClick={goNextoffset}>Next</button>
+			</div>
+		</main>
+	);
 }
 
-export default App
+export default App;
